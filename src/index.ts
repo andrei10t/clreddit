3888bd638c31bb320c118a84dd3fd73803c61aad
andrei10t <andrei.toader32@gmail.com>
2021-09-16 17:18:18 +0300

    added mikroorm entity ex

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./config";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'clreddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+    const post = orm.em.create(Post, {title: "?qwerty" })
+    await orm.em.persistAndFlush(post);
+}
+
+main();
+
 console.log("somet")
